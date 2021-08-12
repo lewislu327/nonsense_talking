64eb8884d0b5b1ffddbc78be82d716056c784399
lewislu327 <agooday32@gmail.com>
2021-08-12 11:41:54 +0800

    typo: fix typo

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const multihelpers = hbshelpers()
 const app = express()
 const port = 3000
 const task = {
-  engineer: ['加個按鈕','加新功能','切個版', '改一點 code'],
-  designer: ['畫一張圖', '改個 logo','順便幫忙設計一下','隨便換個設計'],
-  entrepreneur: ['週末加班', '要能賺錢','想個 business model','找 VC 募錢']
+  engineer: ['加個按鈕', '加新功能', '切個版', '改一點 code'],
+  designer: ['畫一張圖', '改個 logo', '順便幫忙設計一下', '隨便換個設計'],
+  entrepreneur: ['週末加班', '要能賺錢', '想個 business model', '找 VC 募錢']
 }
 
-const phrase = ['很簡單','很容易','很快','很正常']
+const phrase = ['很簡單', '很容易', '很快', '很正常']
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main', helpers: multihelpers}))
+app.engine('handlebars', exphbs({ defaultLayout: 'main', helpers: multihelpers }))
 app.set('view engine', 'handlebars')
 
 app.use(express.urlencoded({ extended: true }))
@@ -24,14 +24,14 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   let bsWord = ''
-  const people = req.body.people 
+  const people = req.body.people
   const tasks = task[people]
   const title = titleSwitch(people)
-  const taskRamdomIndex = Math.floor(Math.random() * tasks.length)
-  const phraseRandomIndex = Math.floor(Math.random() * phrase.length) 
+  const taskRandomIndex = Math.floor(Math.random() * tasks.length)
+  const phraseRandomIndex = Math.floor(Math.random() * phrase.length)
 
-  bsWord = `身為一個${title}，${tasks[taskRamdomIndex]}，${phrase[phraseRandomIndex]}`
-  res.render('index',{bsWord,people} )
+  bsWord = `身為一個${title}，${tasks[taskRandomIndex]}，${phrase[phraseRandomIndex]}`
+  res.render('index', { bsWord, people })
 })
 
 app.listen(port, () => {
@@ -41,10 +41,10 @@ app.listen(port, () => {
 function titleSwitch(people) {
   switch (people) {
     case 'engineer':
-      return '工程師';
+      return '工程師'
     case 'designer':
-      return '設計師';
+      return '設計師'
     case 'entrepreneur':
-      return '創業家';
+      return '創業家'
   }
 }
